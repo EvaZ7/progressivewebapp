01891c58cc842fefdb04ae6522e4c4a726702e00
Eva Zaadnoordijk <evazaadnoordijk@gmail.com>
2023-03-20 17:38:26 +0100

    setup

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require('express')
+import fetch from 'node-fetch';
+import express from 'express';
+import { engine } from 'express-handlebars';
+
 const app = express()
 const port = 3000
 
-const { engine } = require("express-handlebars");
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
@@ -10,8 +12,29 @@ app.set("views", "./views");
 app.use("/static", express.static("static"));
 
 app.get('/', (req, res) => {
-    res.render("test");
-})
+function fetchData() {
+    var category = "amazing";
+    const url1 = `https://api.api-ninjas.com/v1/quotes?category=${category}&limit=10`;
+    const options = {
+      headers: { "X-Api-Key": "mr1axNSXq9DjWUuEBQUHGA==YK6lI63UGnsVpLyt" },
+    };
+  
+    return fetch(url1, options).then((response) => {
+      if (response.status >= 200 && response.status <= 299) {
+        return response.json();
+      } else {
+        throw Error(response.statusText);
+      }
+    });
+  }
+  // fetchData()
+  // .then((data) => {
+  //   console.log(data)
+  //   const dailyquote = data[0].quote
+  //   });
+  // res.render("test", { dailyquote })
+   res.render("test")
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
